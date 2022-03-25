83b4af27627369e76039894c36d80a1e053f72ef
3111727 <3111727@TE-GDA-D-D26F06>
2022-03-25 14:48:30 +0100

    Dodaj pliki projektów.

---
+++ szyfr cezara2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("podaj literke do zakodowania");
+string litera = Console.ReadLine();
+char[] character = litera.ToCharArray();
+int b;
+Console.WriteLine("napisz klucz");
+b = Int32.Parse(Console.ReadLine());
+for (int i = 0; i < character.Length; i++)
+{
+    character[i] = (char)((character[i] + b));
+    if (character[i] > 90)
+    {
+        character[i] = (char)((char)character[i] - 26);
+    }
+}
+string litera2 = new string(character);
+Console.WriteLine("zakodowana wiadomosc to:");
+Console.WriteLine(litera2);
